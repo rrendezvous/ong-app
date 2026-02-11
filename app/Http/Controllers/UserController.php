015122c41a45ed00f6719b33726ee21190e9d895
rrendezvous <joeremychrst@gmail.com>
2026-02-11 08:39:15 +0800

    crud

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // GET /api/users
+    public function index()
+    {
+        return User::all();
+    }
+
+    // POST /api/users
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $data['password'] = bcrypt($data['password']);
+
+        return User::create($data);
+    }
+
+    // GET /api/users/{id}
+    public function show($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    // PUT /api/users/{id}
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $data = $request->validate([
+            'name' => 'sometimes|required|string',
+            'email' => 'sometimes|required|email|unique:users,email,' . $user->id,
+            'password' => 'sometimes|required|string|min:6',
+        ]);
+
+        if (isset($data['password'])) {
+            $data['password'] = bcrypt($data['password']);
+        }
+
+        $user->update($data);
+
+        return $user;
+    }
+
+    // DELETE /api/users/{id}
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'Deleted']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    echo "Been here";
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);         // GET /api/users
+    Route::post('/', [UserController::class, 'store']);        // POST /api/users
+    Route::get('/{id}', [UserController::class, 'show']);      // GET /api/users/{id}
+    Route::put('/{id}', [UserController::class, 'update']);    // PUT /api/users/{id}
+    Route::delete('/{id}', [UserController::class, 'destroy']); // DELETE /api/users/{id}
 });
